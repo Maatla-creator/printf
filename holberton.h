266e2622718d469e51fece589282f4cbe54777f5
Maatla-creator <maatlamog@gmail.com>
2021-07-26 07:08:38 -0700

    header file

---
+++ holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+
+#endif /*holberton.h*/
